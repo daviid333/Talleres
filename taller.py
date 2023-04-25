54ec045cbd40e67287c0a83cf0555ebaa0353d85
David Leal <davidsantiagosuarezleal@gmail.com>
2023-04-25 10:02:35 -0500

    Primer commit

---
+++ taller.py
@@ -0,0 +1,38 @@
+#Ejericicio 1
+ejercicio1 = 40+2*7^2
+print(ejercicio1)
+#Ejercicio 2
+ejercicio2 = 3+(2+5)*1+(3-2*3)
+print(ejercicio2)
+#Ejercicio 3 
+ejercicio3 = 16-8*(10-(5+4))
+print(ejercicio3)
+#Ejercicio 4
+ejercicio4 = (5+3*2)-(7*2)*(3+(2*3-1))
+print(ejercicio4)
+#Ejercicio 5
+ejercicio5 = (24+220)/3*2
+print(ejercicio5)
+#Ejercicio 6
+ejercicio6 = (6/3*2)-(7*2)+ (2*(3+ (2/3-1)*4))
+print(ejercicio6)
+
+## Segundo punto
+#1
+puntodos1 = 29.7 + 5.0 ** 2.0
+print(puntodos1)
+#2
+puntodos2 = ((2-3)**4*5/(4+3*9))
+print(puntodos2)
+#3
+puntodos3 = 49.38+127.73-15.02*6.83/3.22
+print(puntodos3)
+#4
+puntodos4 = 19/3
+print(puntodos4)
+#5
+puntodos5 = 15%6
+print(puntodos5)
+#6
+puntodos6 = ((15*8%6+24/2**3)**3/4)*(5**1/2+1/4+2**3-4)
+print(puntodos6)
