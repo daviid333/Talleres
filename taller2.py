f7c277c3b4698ad8bc10101bd48e9225e928fdfe
David Leal <davidsantiagosuarezleal@gmail.com>
2023-04-25 10:03:56 -0500

    Segundo taller

---
+++ taller2.py
@@ -0,0 +1,58 @@
+# n1 = int(input ("Ingrese numero 1: "))
+# n2 = int(input ("Ingrese numero 2: "))
+
+# if n1>n2:
+#     print(n2, n1)
+# else:
+#     print(n1, n2)
+# ////////////////////////////////////////////////////////////////////
+
+# minuto = float (input("Cuantos minutos demoro su llamda ? "))
+# if minuto <= 3:
+#     print("Su llamada tiene un coste de 200$")
+# elif minuto > 3:
+#     print(200 + ((minuto -3)*100))
+
+# ////////////////////////////////////////////////////////////////////
+
+# c1 = int(input ("Cuantos conejos blancos tienes?"))
+# c2 = int(input ("CUantos conejos negros tienes ?"))
+# y1 = int(input ("Cuantos conejos blancos vendió?"))
+# y2 = int(input ("CUantos conejos negros vendió?"))
+# p1 = 10000
+# p2 = 5000
+# print("Vendiste un total de", y1+y2, "conejos")
+# print("tuviste unas ganancias de " , (y1*p1)+ (y2*p2))
+# if y1 > y2:
+#     print("El color más vendido fue el blanco ")
+# elif y1 == y2:
+#     print("Ambos colores de conejos se vendieron igual")
+# else:
+#     print("EL color mas vendido fue el negro")
+
+# ////////////////////////////////////////////////////////////////////
+
+# nota1 = float(input ("Ingresela primera nota?"))
+# nota2 = float(input ("Ingresela segunda nota?"))
+# nota3 = float(input ("Ingresela tercera nota?"))
+# notaprevios = ((nota1 + nota2 + nota3) /3) * 0.60
+# print("Nota previos:" , notaprevios)
+
+# notat1 = float(input ("Ingrese la nota del primer trabajo"))
+# notat2 = float(input ("Ingrese la nota del segundo trabajo"))
+# notatrabajos = ((notat1 + notat2)/2)*0.40
+# print("Nota trabajos:", notatrabajos)
+
+# notafinal = (notatrabajos + notaprevios)
+# print("Nota final:", notafinal)
+
+# ////////////////////////////////////////////////////////////////////
+
+nombreart = str(input ("Nombre del Articulo: "))
+clave = int(input ("Clave: "))
+precio = float(input ("Precio: "))
+cantidad = int(input ("Cantidad: "))
+
+if clave == 1:
+    
+
